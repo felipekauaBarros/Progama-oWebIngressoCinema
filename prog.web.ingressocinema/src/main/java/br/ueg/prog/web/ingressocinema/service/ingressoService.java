b14d5bbeb885957e0bcc7ad26b3afa0f4d5aea5b
felipekauaBarros <110051743+felipekauaBarros@users.noreply.github.com>
2023-05-14 11:02:55 -0300

    Update ingressoService.java

--- prog.web.ingressocinema/src/main/java/br/ueg/prog/web/ingressocinema/service/ingressoService.java
+++ prog.web.ingressocinema/src/main/java/br/ueg/prog/web/ingressocinema/service/ingressoService.java
@@ -6,7 +6,11 @@ import java.util.List;
 
 public interface ingressoService {
     public cadastroIngresso incluir(cadastroIngresso ingresso);
-    public cadastroIngresso alterar(cadastroIngresso ingresso);
+    public cadastroIngresso alterar(cadastroIngresso ingresso, Long idCodigo);
     public cadastroIngresso excluir(Long idCodigo);
     public List<cadastroIngresso> listarTodos();
+
+    public cadastroIngresso obteringressopelocodigo(Long idCodigo);
+
+    public  List<cadastroIngresso> localizar(cadastroIngresso ingresso);
 }
