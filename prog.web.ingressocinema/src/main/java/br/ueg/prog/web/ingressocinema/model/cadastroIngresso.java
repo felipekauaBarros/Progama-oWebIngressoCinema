12a4a6b6af73e01aebb3d15df1ed032515c8307f
felipekauaBarros <110051743+felipekauaBarros@users.noreply.github.com>
2023-05-18 20:36:10 -0300

    Update cadastroIngresso.java

--- prog.web.ingressocinema/src/main/java/br/ueg/prog/web/ingressocinema/model/cadastroIngresso.java
+++ prog.web.ingressocinema/src/main/java/br/ueg/prog/web/ingressocinema/model/cadastroIngresso.java
@@ -9,28 +9,39 @@ import static jakarta.persistence.GenerationType.SEQUENCE;
 @Entity
 @Table(name = "cadastro_ingresso")
 public class cadastroIngresso {
+    @SequenceGenerator(
+            name="ingresso_sequence",
+            sequenceName = "ingresso_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "ingresso_sequence"
+    )
+
     @Id
-    @Column(name = "codigo")
+    @Column(name = "IdCodigo")
     private Long idCodigo;
 
     @Column(name = "nome_do_cliente", length = 200, nullable = false)
     private String nomeCliente;
 
-    @Column(name = "sala_do_filme", length = 20, nullable = false)
+    @Column(name = "sala_do_filme", length = 20)
     private String salaFime;
 
     @Column(name = "assento_da_sala", length = 6, nullable = false)
 private String assentoSala;
 
-    @Column(name = "tipo_do_ingresso", length = 10, nullable = false)
+    @Column(name = "tipo_do_ingresso", length = 10)
     private String tipoIngresso;
 
-    @Column(name = "data_hora_do_filme", length = 20, nullable = false)
+    @Column(name = "data_hora_do_filme", length = 20)
     private String data_horaFilme;
 
-    @Column(name = "categoria_do_filme", length = 50, nullable = false)
+    @Column(name = "categoria_do_filme", length = 50)
     private String categoriaFilme;
 
-    @Column(name = "nome_do_filme", length = 200, nullable = false)
+    @Column(name = "nome_do_filme", length = 200)
     private String nomeFilme;
 }
